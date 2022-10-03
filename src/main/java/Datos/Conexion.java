67bf028ee9847287b3e239cec9aa4e20fd69f73b
Felipe Alferez <alferezfelipe@gmail.com>
2022-10-02 22:25:33 -0500

    cambios BD2

--- src/main/java/Datos/Conexion.java
+++ src/main/java/Datos/Conexion.java
@@ -12,13 +12,13 @@ import java.sql.*;
  * @author FELIPE
  */
 public class Conexion {
-    private static final String JDBC_URL = "jdbc:mysql://localhost:3306/tareas";
+    private static final String JDBC_URL = "jdbc:mysql://root:VdSuZkuXVJUqdprPrmZl@containers-us-west-79.railway.app:6141/railway";
     
     public static Connection getConnection(){
         Connection conexion = null;
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
-            conexion =  DriverManager.getConnection(JDBC_URL, "root", "");
+            conexion =  DriverManager.getConnection(JDBC_URL, "root", "VdSuZkuXVJUqdprPrmZl");
             System.out.println("Database Connected");
         } catch (Exception e) {
             System.out.println(e.getMessage());
